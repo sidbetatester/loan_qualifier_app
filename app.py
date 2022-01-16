ce910173c5c7a21d789500d878f51d53b4d8c095
Sid V <94043668+sidbetatester@users.noreply.github.com>
2022-01-16 01:51:15 -0800

    Usability: Updated the CLI to show user prompts for saving results and verified success criteria

--- app.py
+++ app.py
@@ -116,10 +116,24 @@ def save_qualifying_loans(qualifying_loans):
     """
     # @TODO: Complete the usability dialog for savings the CSV Files.
     # YOUR CODE HERE!
-    new_csv_path_and_filename = "qualifing_loan_data_2.csv"
-    save_csv(qualifying_loans, new_csv_path_and_filename)
 
 
+    # if qualifying loans exist, then 
+    if len(qualifying_loans) > 0:        
+     
+    # The tool should prompt the user to save the results as a CSV file, if user opts to save, then prompt them to provide path and filename for the new CSV.
+
+        Save_the_Data = questionary.confirm("Would you like to save the results to a CSV file?").ask()
+        if Save_the_Data:
+            new_csv_path_and_filename = questionary.text("Please provide the path and name of your file in name.csv format").ask()
+            save_csv(qualifying_loans, new_csv_path_and_filename)
+
+        else:
+            print("You have opted not to save the loan data, thank you for using the App")
+
+    # if there are no qualifying loans, the program should end without promting to save data
+    else:
+        print("You have no qualifying loans, thank you for using the App")
 
 
 def run():
